79342e03ef9ba2be79a5d237c7c69ebbf7998e76
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-09-27 23:10:53 -0300

    main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+app.listen(process.env.PORT || port);
 
 app.set("view engine", "ejs");
 
